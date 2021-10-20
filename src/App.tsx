c974212a188f66514c0b78512ad48b3f9ddfc897
Thomas Bouvier <tombvr75@gmail.com>
2021-10-20 16:15:28 +0100

    scss install + basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+
+import InputFields from './components/InputFields'
 
 const App = () => {
+
+  const [todo, setTodo] = useState<string>("")
+
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <span className="header">YOU HAVE SOME WORK BOY</span>
+      <InputFields todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
