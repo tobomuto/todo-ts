05a5c550afa536001583f0452fb93b5fc8810c71
Thomas Bouvier <tombvr75@gmail.com>
2021-10-21 13:28:57 +0100

    TodoItem styling

--- src/components/TodoElement.tsx
+++ src/components/TodoElement.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './styles/TodoElement.scss';
+import {RiDeleteBin7Fill, RiEdit2Fill, RiCheckFill} from 'react-icons/ri'
+
 
 import { Todo } from '../model';
 
@@ -12,7 +14,12 @@ interface Props {
 const TodoElement = ({todo, todos, setTodos}: Props) => {
     return (
         <form className="todo-element">
-            
+            <span className="todo-element__text">{todo.todo}</span>
+            <div className="todo-element__icons">
+                <span className="todo-element__icon"><RiCheckFill/></span>
+                <span className="todo-element__icon"><RiEdit2Fill /></span>
+                <span className="todo-element__icon"><RiDeleteBin7Fill /></span>
+            </div>
         </form>
     )
 }
