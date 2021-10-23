4a82568e30920658894285ffa44d58609d77e926
Thomas Bouvier <tombvr75@gmail.com>
2021-10-23 15:17:23 +0100

    handle Delete function

--- src/components/TodoElement.tsx
+++ src/components/TodoElement.tsx
@@ -13,7 +13,10 @@ interface Props {
 
 const TodoElement = ({todo, todos, setTodos}: Props) => {
     const handleDone = (id:number) => {
-        setTodos(todos.map(todo => todo.id===id ? { ...todo, isDone: !todo.isDone } : todo))
+        setTodos(todos.map(todo => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
+    }
+    const handleDelete = (id:number) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
     }
     return (
         <form className="todo-element">
@@ -27,7 +30,7 @@ const TodoElement = ({todo, todos, setTodos}: Props) => {
             <div className="todo-element__icons">
                 <span className="todo-element__icon" onClick={() => handleDone(todo.id)}><RiCheckFill/></span>
                 <span className="todo-element__icon"><RiEdit2Fill /></span>
-                <span className="todo-element__icon"><RiDeleteBin7Fill /></span>
+                <span className="todo-element__icon" onClick={() => handleDelete(todo.id)}><RiDeleteBin7Fill /></span>
             </div>
         </form>
     )
